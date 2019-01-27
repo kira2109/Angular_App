ef092a83a95855df7adcd020b6a254ae1a215954
a.tishchenko <a.tishchenko@outlook.com>
2019-01-27 23:13:02 +0200

    Add logic to 'Show image' button, using event binding

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,9 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
   pageTitle: string = "Product List";
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
   products: any[] = [
     {
       productId: 1,
@@ -63,4 +66,8 @@ export class ProductListComponent {
         "https://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png"
     }
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
